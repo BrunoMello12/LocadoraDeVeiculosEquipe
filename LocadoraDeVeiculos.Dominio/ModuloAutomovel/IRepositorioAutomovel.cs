148c8d744671eb8b285f09ccd98aa2bdfe2bd44e
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-07-31 15:36:05 -0300

    Implementando Aplicação do Automóvel

--- LocadoraDeVeiculos.Dominio/ModuloAutomovel/IRepositorioAutomovel.cs
+++ LocadoraDeVeiculos.Dominio/ModuloAutomovel/IRepositorioAutomovel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,9 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.ModuloAutomovel
 {
-    public interface IRepositorioAutomovel
+    public interface IRepositorioAutomovel : IRepositorio<Automovel>
     {
+        public Automovel SelecionarPorId(int id, bool incluirGrupoDoAutomovel = false);
+        public List<Automovel> SelecionarTodos(bool incluirGrupoDoAutomovel = false);
     }
 }
