9aa9d0e4c2aaed6df4dd0cf26e177bbc32f7f86b
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-07-31 12:39:53 -0300

    Interface

--- LocadoraDeVeiculos.Dominio/ModuloAutomovel/IRepositorioAutomovel.cs
+++ LocadoraDeVeiculos.Dominio/ModuloAutomovel/IRepositorioAutomovel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.ModuloAutomovel
 {
-    public class IRepositorioAutomovel
+    public interface IRepositorioAutomovel
     {
     }
 }
