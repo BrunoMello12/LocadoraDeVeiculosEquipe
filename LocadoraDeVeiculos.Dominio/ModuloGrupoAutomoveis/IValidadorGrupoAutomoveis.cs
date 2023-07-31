73c54c44c1550a10a7f438d909f9cf1157bbe019
Bruno <Cliente@DESKTOP-2TBK80U>
2023-07-31 14:21:28 -0300

    Corringindo bug do merge

--- LocadoraDeVeiculos.Dominio/ModuloGrupoAutomoveis/IValidadorGrupoAutomoveis.cs
+++ LocadoraDeVeiculos.Dominio/ModuloGrupoAutomoveis/IValidadorGrupoAutomoveis.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.ModuloGrupoAutomoveis
 {
-    internal interface IValidadorGrupoAutomoveis
+    public interface IValidadorGrupoAutomoveis : IValidator<GrupoAutomoveis>
     {
     }
 }
