2a1bb2184114013ebcb344a08f32104ab1cb9e25
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-07-31 15:58:44 -0300

    Iniciando Implementação da tela De Automovel

--- LocadoraDeVeiculos.WinFormsApp/ModuloAutomovel/TelaAutomovelForm.cs
+++ LocadoraDeVeiculos.WinFormsApp/ModuloAutomovel/TelaAutomovelForm.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.ModuloAutomovel;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoAutomoveis;
+using LocadoraDeVeiculos.WinFormsApp.Compartilhado;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,38 @@ namespace LocadoraDeVeiculos.WinFormsApp.ModuloAutomovel
 {
     public partial class TelaAutomovelForm : Form
     {
-        public TelaAutomovelForm()
+        public TelaAutomovelForm(List<GrupoAutomoveis> grupoDeAutomoveis)
         {
             InitializeComponent();
+            this.ConfigurarDialog();
+            CarregarGrupoDeAltomoveis(grupoDeAutomoveis);
+            CarregarTipoCombustivel();
+        }
+
+        private void CarregarTipoCombustivel()
+        {
+            TipoCombustivelEnum[] tiposCombustiveis = Enum.GetValues<TipoCombustivelEnum>();
+
+            ArrayList items = new ArrayList();
+
+            foreach (Enum tipoCombustivel in tiposCombustiveis)
+            {
+                items.Add(tipoCombustivel);
+            }
+
+            cbTipoCombustivel.DataSource = items;
+            //cbTipoCombustivel.DisplayMember = "Value";
+            //cbTipoCombustivel.ValueMember = "Key";
+        }
+
+        private void CarregarGrupoDeAltomoveis(List<GrupoAutomoveis> grupoDeAutomoveis)
+        {
+            cbGrpAutomoveis.Items.Clear();
+
+            foreach (var item in grupoDeAutomoveis)
+            {
+                cbGrpAutomoveis.Items.Add(item);
+            }
         }
     }
 }
