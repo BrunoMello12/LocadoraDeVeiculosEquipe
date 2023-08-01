5ecc4276f2c6eb7c58d87df911ab59ac6c8b571d
GustavoRafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-08-01 17:34:14 -0300

    Percebendo que é dificil a valdiação do Aluguel

--- LocadoraDeVeiculos.Dominio/ModuloAluguel/ValidadorAluguel.cs
+++ LocadoraDeVeiculos.Dominio/ModuloAluguel/ValidadorAluguel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FluentValidation;
+using LocadoraDeVeiculos.Dominio.ModuloAutomovel;
+using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,35 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.ModuloAluguel
 {
-    internal class ValidadorAluguel
+    public class ValidadorAluguel : AbstractValidator<Aluguel>
     {
+        public ValidadorAluguel()
+        {
+            RuleFor(x => x.Condutor)
+                .NotNull().NotEmpty();
+
+            RuleFor(x => x.GrupoAutomoveis)
+                .NotNull().NotEmpty(); 
+
+            RuleFor(x => x.Cobranca)
+                .NotNull().NotEmpty(); 
+
+            RuleFor(x => x.Automovel)
+                .NotEmpty().NotEmpty();
+
+            RuleFor(x => x.DataLocacao)
+                .NotNull().NotEmpty();
+
+            RuleFor(x => x.DevolucaoPrevista)
+                .NotEmpty().NotEmpty();
+        }
+
+        //public void ClienteJuridicoDevePossuirCondutorPF(Cliente cliente)
+        //{
+        //    if(cliente.TipoCliente == TipoClienteEnum.PessoaJuridica && cliente)
+        //    {
+
+        //    }
+        //}
     }
 }
